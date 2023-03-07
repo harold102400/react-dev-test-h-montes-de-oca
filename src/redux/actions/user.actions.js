6fa71a91c93dacd452fb7374440157c205327524
Harold Montes <haroldjosehd24@gmail.com>
2023-03-07 12:07:50 -0500

    Organize code structure and file order

--- src/redux/actions/user.actions.js
+++
@@ -1,7 +0,0 @@
-import { types } from "../types";
-
-export const addUser = ({ name, firstLastname, secondLastname, id, age, gender, address, phone, email, maritalStatus, children, birthday }) => ({
-    type: types.addUser,
-    payload: { name, firstLastname, secondLastname, id, age, gender, address, phone, email, maritalStatus, children, birthday },
-  });
-  
